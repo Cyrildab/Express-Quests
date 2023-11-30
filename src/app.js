265d77d97d79b3dc622ffdd6eba2a3431d245554
CYRIL DABURON <cyril.daburon@gmail.com>
2023-11-30 11:26:51 +0100

    quest done

--- src/app.js
+++ src/app.js
@@ -15,5 +15,7 @@ app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
 app.get("/api/users", usersController.getUsers);
 app.get("/api/users/:id", usersController.getUsersById);
 app.put("/api/users/:id", validateUser, usersController.updateUser);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
+app.delete("/api/users/:id", usersController.deleteUser);
 
 module.exports = app;
